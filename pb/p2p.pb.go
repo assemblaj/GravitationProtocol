6a4cd5555c4fcc56ac23f3da10e089831d78a52c
arheard <arheard0@gmail.com>
2019-01-07 00:36:32 -0500

    Fixes protobufs schema

--- pb/p2p.pb.go
+++ pb/p2p.pb.go
@@ -110,8 +110,9 @@ func (m *MessageData) GetSign() []byte {
 
 // a protocol define a set of reuqest and responses
 type GravitationRequest struct {
-	MessageData          *MessageData                   `protobuf:"bytes,1,opt,name=messageData,proto3" json:"messageData,omitempty"`
-	Profile              []*GravitationRequest_Profile  `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
+	MessageData *MessageData `protobuf:"bytes,1,opt,name=messageData,proto3" json:"messageData,omitempty"`
+	// method specific data
+	Profile              []string                       `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
 	SubOrbit             []*GravitationRequest_SubOrbit `protobuf:"bytes,3,rep,name=sub_orbit,json=subOrbit,proto3" json:"sub_orbit,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}                       `json:"-"`
 	XXX_unrecognized     []byte                         `json:"-"`
@@ -149,7 +150,7 @@ func (m *GravitationRequest) GetMessageData() *MessageData {
 	return nil
 }
 
-func (m *GravitationRequest) GetProfile() []*GravitationRequest_Profile {
+func (m *GravitationRequest) GetProfile() []string {
 	if m != nil {
 		return m.Profile
 	}
@@ -163,58 +164,19 @@ func (m *GravitationRequest) GetSubOrbit() []*GravitationRequest_SubOrbit {
 	return nil
 }
 
-// method specific data
-type GravitationRequest_Profile struct {
-	Item                 string   `protobuf:"bytes,1,opt,name=item,proto3" json:"item,omitempty"`
+type GravitationRequest_SubOrbit struct {
+	PeerId               string   `protobuf:"bytes,1,opt,name=peer_id,json=peerId,proto3" json:"peer_id,omitempty"`
+	Profile              []string `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *GravitationRequest_Profile) Reset()         { *m = GravitationRequest_Profile{} }
-func (m *GravitationRequest_Profile) String() string { return proto.CompactTextString(m) }
-func (*GravitationRequest_Profile) ProtoMessage()    {}
-func (*GravitationRequest_Profile) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e7fdddb109e6467a, []int{1, 0}
-}
-func (m *GravitationRequest_Profile) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_GravitationRequest_Profile.Unmarshal(m, b)
-}
-func (m *GravitationRequest_Profile) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_GravitationRequest_Profile.Marshal(b, m, deterministic)
-}
-func (m *GravitationRequest_Profile) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GravitationRequest_Profile.Merge(m, src)
-}
-func (m *GravitationRequest_Profile) XXX_Size() int {
-	return xxx_messageInfo_GravitationRequest_Profile.Size(m)
-}
-func (m *GravitationRequest_Profile) XXX_DiscardUnknown() {
-	xxx_messageInfo_GravitationRequest_Profile.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_GravitationRequest_Profile proto.InternalMessageInfo
-
-func (m *GravitationRequest_Profile) GetItem() string {
-	if m != nil {
-		return m.Item
-	}
-	return ""
-}
-
-type GravitationRequest_SubOrbit struct {
-	PeerId               string                        `protobuf:"bytes,1,opt,name=peer_id,json=peerId,proto3" json:"peer_id,omitempty"`
-	Profile              []*GravitationRequest_Profile `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}                      `json:"-"`
-	XXX_unrecognized     []byte                        `json:"-"`
-	XXX_sizecache        int32                         `json:"-"`
 }
 
 func (m *GravitationRequest_SubOrbit) Reset()         { *m = GravitationRequest_SubOrbit{} }
 func (m *GravitationRequest_SubOrbit) String() string { return proto.CompactTextString(m) }
 func (*GravitationRequest_SubOrbit) ProtoMessage()    {}
 func (*GravitationRequest_SubOrbit) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e7fdddb109e6467a, []int{1, 1}
+	return fileDescriptor_e7fdddb109e6467a, []int{1, 0}
 }
 func (m *GravitationRequest_SubOrbit) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GravitationRequest_SubOrbit.Unmarshal(m, b)
@@ -241,7 +203,7 @@ func (m *GravitationRequest_SubOrbit) GetPeerId() string {
 	return ""
 }
 
-func (m *GravitationRequest_SubOrbit) GetProfile() []*GravitationRequest_Profile {
+func (m *GravitationRequest_SubOrbit) GetProfile() []string {
 	if m != nil {
 		return m.Profile
 	}
@@ -249,8 +211,9 @@ func (m *GravitationRequest_SubOrbit) GetProfile() []*GravitationRequest_Profile
 }
 
 type GravitationResponse struct {
-	MessageData          *MessageData                    `protobuf:"bytes,1,opt,name=messageData,proto3" json:"messageData,omitempty"`
-	Profile              []*GravitationResponse_Profile  `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
+	MessageData *MessageData `protobuf:"bytes,1,opt,name=messageData,proto3" json:"messageData,omitempty"`
+	// method specific data
+	Profile              []string                        `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
 	SubOrbit             []*GravitationResponse_SubOrbit `protobuf:"bytes,3,rep,name=sub_orbit,json=subOrbit,proto3" json:"sub_orbit,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}                        `json:"-"`
 	XXX_unrecognized     []byte                          `json:"-"`
@@ -288,7 +251,7 @@ func (m *GravitationResponse) GetMessageData() *MessageData {
 	return nil
 }
 
-func (m *GravitationResponse) GetProfile() []*GravitationResponse_Profile {
+func (m *GravitationResponse) GetProfile() []string {
 	if m != nil {
 		return m.Profile
 	}
@@ -302,58 +265,19 @@ func (m *GravitationResponse) GetSubOrbit() []*GravitationResponse_SubOrbit {
 	return nil
 }
 
-// method specific data
-type GravitationResponse_Profile struct {
-	Item                 string   `protobuf:"bytes,1,opt,name=item,proto3" json:"item,omitempty"`
+type GravitationResponse_SubOrbit struct {
+	PeerId               string   `protobuf:"bytes,1,opt,name=peer_id,json=peerId,proto3" json:"peer_id,omitempty"`
+	Profile              []string `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *GravitationResponse_Profile) Reset()         { *m = GravitationResponse_Profile{} }
-func (m *GravitationResponse_Profile) String() string { return proto.CompactTextString(m) }
-func (*GravitationResponse_Profile) ProtoMessage()    {}
-func (*GravitationResponse_Profile) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e7fdddb109e6467a, []int{2, 0}
-}
-func (m *GravitationResponse_Profile) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_GravitationResponse_Profile.Unmarshal(m, b)
-}
-func (m *GravitationResponse_Profile) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_GravitationResponse_Profile.Marshal(b, m, deterministic)
-}
-func (m *GravitationResponse_Profile) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GravitationResponse_Profile.Merge(m, src)
-}
-func (m *GravitationResponse_Profile) XXX_Size() int {
-	return xxx_messageInfo_GravitationResponse_Profile.Size(m)
-}
-func (m *GravitationResponse_Profile) XXX_DiscardUnknown() {
-	xxx_messageInfo_GravitationResponse_Profile.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_GravitationResponse_Profile proto.InternalMessageInfo
-
-func (m *GravitationResponse_Profile) GetItem() string {
-	if m != nil {
-		return m.Item
-	}
-	return ""
-}
-
-type GravitationResponse_SubOrbit struct {
-	PeerId               string                         `protobuf:"bytes,1,opt,name=peer_id,json=peerId,proto3" json:"peer_id,omitempty"`
-	Profile              []*GravitationResponse_Profile `protobuf:"bytes,2,rep,name=profile,proto3" json:"profile,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}                       `json:"-"`
-	XXX_unrecognized     []byte                         `json:"-"`
-	XXX_sizecache        int32                          `json:"-"`
 }
 
 func (m *GravitationResponse_SubOrbit) Reset()         { *m = GravitationResponse_SubOrbit{} }
 func (m *GravitationResponse_SubOrbit) String() string { return proto.CompactTextString(m) }
 func (*GravitationResponse_SubOrbit) ProtoMessage()    {}
 func (*GravitationResponse_SubOrbit) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e7fdddb109e6467a, []int{2, 1}
+	return fileDescriptor_e7fdddb109e6467a, []int{2, 0}
 }
 func (m *GravitationResponse_SubOrbit) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GravitationResponse_SubOrbit.Unmarshal(m, b)
@@ -380,7 +304,7 @@ func (m *GravitationResponse_SubOrbit) GetPeerId() string {
 	return ""
 }
 
-func (m *GravitationResponse_SubOrbit) GetProfile() []*GravitationResponse_Profile {
+func (m *GravitationResponse_SubOrbit) GetProfile() []string {
 	if m != nil {
 		return m.Profile
 	}
@@ -390,39 +314,34 @@ func (m *GravitationResponse_SubOrbit) GetProfile() []*GravitationResponse_Profi
 func init() {
 	proto.RegisterType((*MessageData)(nil), "protocols.p2p.MessageData")
 	proto.RegisterType((*GravitationRequest)(nil), "protocols.p2p.GravitationRequest")
-	proto.RegisterType((*GravitationRequest_Profile)(nil), "protocols.p2p.GravitationRequest.Profile")
 	proto.RegisterType((*GravitationRequest_SubOrbit)(nil), "protocols.p2p.GravitationRequest.SubOrbit")
 	proto.RegisterType((*GravitationResponse)(nil), "protocols.p2p.GravitationResponse")
-	proto.RegisterType((*GravitationResponse_Profile)(nil), "protocols.p2p.GravitationResponse.Profile")
 	proto.RegisterType((*GravitationResponse_SubOrbit)(nil), "protocols.p2p.GravitationResponse.SubOrbit")
 }
 
 func init() { proto.RegisterFile("p2p.proto", fileDescriptor_e7fdddb109e6467a) }
 
 var fileDescriptor_e7fdddb109e6467a = []byte{
-	// 369 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x91, 0x4f, 0x4b, 0xf3, 0x40,
-	0x10, 0xc6, 0x49, 0xd2, 0x37, 0x69, 0x26, 0x6f, 0x3d, 0xac, 0xa0, 0x4b, 0x51, 0x09, 0xc5, 0x43,
-	0x54, 0xc8, 0x21, 0x5e, 0xbd, 0x59, 0xa8, 0x45, 0xc4, 0xb2, 0x82, 0xd7, 0x92, 0x34, 0x6b, 0x5d,
-	0x68, 0xb2, 0x6b, 0x76, 0x23, 0xf8, 0x01, 0xfd, 0x30, 0x1e, 0xfc, 0x0e, 0x92, 0x6d, 0x4a, 0xff,
-	0x04, 0xb4, 0x62, 0x4f, 0x99, 0x99, 0xcc, 0xf3, 0x4c, 0x9e, 0x5f, 0xc0, 0x15, 0x91, 0x08, 0x45,
-	0xc1, 0x15, 0x47, 0x1d, 0xfd, 0x98, 0xf0, 0x99, 0x0c, 0x45, 0x24, 0x7a, 0xef, 0x06, 0x78, 0x77,
-	0x54, 0xca, 0x78, 0x4a, 0xfb, 0xb1, 0x8a, 0xd1, 0x29, 0x74, 0x26, 0x33, 0x46, 0x73, 0xf5, 0x48,
-	0x0b, 0xc9, 0x78, 0x8e, 0x0d, 0xdf, 0x08, 0x5c, 0xb2, 0x3e, 0x44, 0x47, 0xe0, 0x2a, 0x96, 0x51,
-	0xa9, 0xe2, 0x4c, 0x60, 0xd3, 0x37, 0x02, 0x8b, 0x2c, 0x07, 0x68, 0x0f, 0x4c, 0x96, 0x62, 0x4b,
-	0x0b, 0x4d, 0x96, 0xa2, 0x03, 0xb0, 0xa7, 0x5c, 0x4a, 0x26, 0x70, 0xcb, 0x37, 0x82, 0x36, 0xa9,
-	0xbb, 0x6a, 0x9e, 0xf3, 0x94, 0x0e, 0x53, 0xfc, 0x4f, 0xef, 0xd6, 0x1d, 0x3a, 0x01, 0xa8, 0xaa,
-	0x51, 0x99, 0xdc, 0xd2, 0x37, 0x6c, 0xfb, 0x46, 0xf0, 0x9f, 0xac, 0x4c, 0x10, 0x82, 0x96, 0x64,
-	0xd3, 0x1c, 0x3b, 0xfa, 0x8d, 0xae, 0x7b, 0x1f, 0x26, 0xa0, 0x41, 0x11, 0xbf, 0x32, 0x15, 0x2b,
-	0xc6, 0x73, 0x42, 0x5f, 0x4a, 0x2a, 0x15, 0xba, 0x02, 0x2f, 0x5b, 0xa6, 0xd3, 0x61, 0xbc, 0xa8,
-	0x1b, 0xae, 0x31, 0x08, 0x57, 0xf2, 0x93, 0xd5, 0x75, 0x74, 0x0d, 0x8e, 0x28, 0xf8, 0x13, 0x9b,
-	0x51, 0x6c, 0xfa, 0x56, 0xe0, 0x45, 0x67, 0x1b, 0xca, 0xe6, 0xc5, 0x70, 0x34, 0x17, 0x90, 0x85,
-	0x12, 0x0d, 0xc0, 0x95, 0x65, 0x32, 0xe6, 0x45, 0xc2, 0x14, 0xb6, 0xb4, 0xcd, 0xf9, 0xcf, 0x36,
-	0x0f, 0x65, 0x72, 0x5f, 0x29, 0x48, 0x5b, 0xd6, 0x55, 0xf7, 0x18, 0x9c, 0xda, 0xbc, 0x22, 0xc0,
-	0x14, 0xcd, 0xea, 0x9f, 0xa3, 0xeb, 0xee, 0x33, 0xb4, 0x17, 0x22, 0x74, 0x08, 0x8e, 0xa0, 0xb4,
-	0x18, 0xb3, 0xb4, 0x5e, 0xb1, 0xab, 0x76, 0x98, 0xee, 0x24, 0x51, 0xef, 0xd3, 0x84, 0xfd, 0xb5,
-	0x3d, 0x29, 0x78, 0x2e, 0xe9, 0x1f, 0x61, 0xf7, 0x37, 0x3f, 0xed, 0x5b, 0x4a, 0xf3, 0x93, 0x4d,
-	0xda, 0x37, 0x4d, 0xda, 0x17, 0x5b, 0xf8, 0xfc, 0x1e, 0x37, 0xdb, 0x06, 0xf7, 0x4e, 0x32, 0x25,
-	0xb6, 0xd6, 0x5c, 0x7e, 0x05, 0x00, 0x00, 0xff, 0xff, 0x09, 0xa4, 0x6d, 0xd2, 0xc6, 0x03, 0x00,
-	0x00,
+	// 331 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x90, 0x41, 0x4b, 0xc3, 0x40,
+	0x10, 0x85, 0xd9, 0xa4, 0xa6, 0xcd, 0xc4, 0x7a, 0x58, 0x41, 0x97, 0x22, 0x12, 0x8a, 0x87, 0xa0,
+	0x90, 0x43, 0xbc, 0xea, 0x4d, 0xa8, 0x45, 0x44, 0x59, 0xc1, 0x6b, 0x49, 0x9a, 0xb1, 0x2c, 0xb4,
+	0xd9, 0x35, 0xb3, 0x11, 0xfc, 0x81, 0xfe, 0x23, 0xf1, 0x2c, 0x89, 0x29, 0x6d, 0x2d, 0x7a, 0xd2,
+	0x53, 0xe6, 0xbd, 0xcc, 0xec, 0xbc, 0x6f, 0xc0, 0x37, 0x89, 0x89, 0x4d, 0xa9, 0xad, 0xe6, 0xfd,
+	0xe6, 0x33, 0xd5, 0x73, 0x8a, 0x4d, 0x62, 0x86, 0x6f, 0x0c, 0x82, 0x5b, 0x24, 0x4a, 0x67, 0x78,
+	0x95, 0xda, 0x94, 0x9f, 0x40, 0x7f, 0x3a, 0x57, 0x58, 0xd8, 0x47, 0x2c, 0x49, 0xe9, 0x42, 0xb0,
+	0x90, 0x45, 0xbe, 0xdc, 0x34, 0xf9, 0x11, 0xf8, 0x56, 0x2d, 0x90, 0x6c, 0xba, 0x30, 0xc2, 0x09,
+	0x59, 0xe4, 0xca, 0x95, 0xc1, 0xf7, 0xc0, 0x51, 0xb9, 0x70, 0x9b, 0x41, 0x47, 0xe5, 0xfc, 0x00,
+	0xbc, 0x99, 0x26, 0x52, 0x46, 0x74, 0x42, 0x16, 0xf5, 0x64, 0xab, 0x6a, 0xbf, 0xd0, 0x39, 0x8e,
+	0x73, 0xb1, 0xd3, 0xf4, 0xb6, 0x8a, 0x1f, 0x03, 0xd4, 0xd5, 0x7d, 0x95, 0xdd, 0xe0, 0xab, 0xf0,
+	0x42, 0x16, 0xed, 0xca, 0x35, 0x87, 0x73, 0xe8, 0x90, 0x9a, 0x15, 0xa2, 0xdb, 0xfc, 0x69, 0xea,
+	0xe1, 0x3b, 0x03, 0x3e, 0x2a, 0xd3, 0x17, 0x65, 0x53, 0xab, 0x74, 0x21, 0xf1, 0xb9, 0x42, 0xb2,
+	0xfc, 0x02, 0x82, 0xc5, 0x8a, 0xae, 0x81, 0x09, 0x92, 0x41, 0xbc, 0x71, 0x83, 0x78, 0x8d, 0x5f,
+	0xae, 0xb7, 0x73, 0x01, 0x5d, 0x53, 0xea, 0x27, 0x35, 0x47, 0xe1, 0x84, 0x6e, 0xe4, 0xcb, 0xa5,
+	0xe4, 0x23, 0xf0, 0xa9, 0xca, 0x26, 0xba, 0xcc, 0x94, 0x15, 0x6e, 0xe8, 0x46, 0x41, 0x72, 0xfa,
+	0xed, 0xd5, 0xed, 0x34, 0xf1, 0x43, 0x95, 0xdd, 0xd5, 0x13, 0xb2, 0x47, 0x6d, 0x35, 0xb8, 0x84,
+	0xde, 0xd2, 0xe5, 0x87, 0xd0, 0x35, 0x88, 0xe5, 0x44, 0xe5, 0xed, 0xd5, 0xbd, 0x5a, 0x8e, 0xf3,
+	0x9f, 0x73, 0x0c, 0x3f, 0x18, 0xec, 0x6f, 0x2c, 0x22, 0xa3, 0x0b, 0xc2, 0x7f, 0xe3, 0xbe, 0xde,
+	0xe6, 0x3e, 0xfb, 0x8d, 0xfb, 0x2b, 0xce, 0xdf, 0x83, 0x67, 0x5e, 0xb3, 0xf4, 0xfc, 0x33, 0x00,
+	0x00, 0xff, 0xff, 0x8d, 0x90, 0x12, 0x2e, 0xda, 0x02, 0x00, 0x00,
 }
