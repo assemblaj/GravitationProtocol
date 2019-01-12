335ce3ed3d74e54890b7ac9b8a6056c2ef1977fe
arheard <arheard0@gmail.com>
2019-01-11 21:06:42 -0500

    Adds starter rendezvous code

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"reflect"
 	"sort"
+	"sync"
 	"time"
 
 	libp2p "github.com/libp2p/go-libp2p"
 	crypto "github.com/libp2p/go-libp2p-crypto"
+	discovery "github.com/libp2p/go-libp2p-discovery"
+	libp2pdht "github.com/libp2p/go-libp2p-kad-dht"
+	peerstore "github.com/libp2p/go-libp2p-peerstore"
 	ps "github.com/libp2p/go-libp2p-peerstore"
 	ma "github.com/multiformats/go-multiaddr"
+	multiaddr "github.com/multiformats/go-multiaddr"
 )
 
 // helper method - create a lib-p2p host to listen on a port
@@ -117,6 +121,99 @@ func testGravitation(fname string) bool {
 	return reflect.DeepEqual(actualOrbitIds, orbitPeerIds)
 }
 
+func gravitationRendezvous(profile []string, orbit []Body) {
+	done := make(chan bool, 1)
+
+	ctx := context.Background()
+	//host := makeRandomNode(port, done, []string{profile}, []Body{})
+
+	config, err := ParseFlags()
+	if err != nil {
+		panic(err)
+	}
+
+	// libp2p.New constructs a new libp2p Host. Other options can be added
+	// here.
+	priv, _, _ := crypto.GenerateKeyPair(crypto.Secp256k1, 256)
+	//listen, _ := ma.NewMultiaddr(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", port))
+
+	host, err := libp2p.New(
+		context.Background(),
+		libp2p.ListenAddrs([]multiaddr.Multiaddr(config.ListenAddresses)...),
+		libp2p.Identity(priv),
+		libp2p.DisableRelay(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	node := NewNode(host, done, profile, []Body{})
+
+	// ----------------------------
+	// Start a DHT, for use in peer discovery. We can't just make a new DHT
+	// client because we want each peer to maintain its own local copy of the
+	// DHT, so that the bootstrapping node of the DHT can go down without
+	// inhibiting future peer discovery.
+	kademliaDHT, err := libp2pdht.New(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+
+	// Bootstrap the DHT. In the default configuration, this spawns a Background
+	// thread that will refresh the peer table every five minutes.
+	log.Println("Bootstrapping the DHT")
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+
+	// Let's connect to the bootstrap nodes first. They will tell us about the
+	// other nodes in the network.
+	var wg sync.WaitGroup
+	for _, peerAddr := range config.BootstrapPeers {
+		peerinfo, _ := peerstore.InfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerinfo); err != nil {
+				log.Println(err)
+			} else {
+				log.Println("Connection established with bootstrap node:", *peerinfo)
+			}
+		}()
+	}
+	wg.Wait()
+
+	// We use a rendezvous point "meet me here" to announce our location.
+	// This is like telling your friends to meet you at the Eiffel Tower.
+	log.Println("Announcing ourselves...")
+	routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
+	discovery.Advertise(ctx, routingDiscovery, config.RendezvousString)
+	log.Println("Successfully announced!")
+
+	// Now, look for others who have announced
+	// This is like your friend telling you the location to meet you.
+	log.Println("Searching for other peers...")
+	peerChan, err := routingDiscovery.FindPeers(ctx, config.RendezvousString)
+	if err != nil {
+		panic(err)
+	}
+
+	for peer := range peerChan {
+		if peer.ID == host.ID() {
+			continue
+		}
+		log.Println("Found peer:", peer)
+
+		log.Println("Connecting to:", peer)
+		//stream, err := host.NewStream(ctx, peer.ID, protocol.ID(config.ProtocolID))
+		go node.GravitationPeerID(peer.ID)
+
+		log.Println("Connected to:", peer)
+	}
+
+	select {}
+}
+
 const help = `
 Creates Gravitation protocol instance. 
 Usage: 
@@ -127,22 +224,25 @@ Usage:
 `
 
 func main() {
-	flag.Usage = func() {
-		fmt.Println(help)
-		flag.PrintDefaults()
-	}
+	// flag.Usage = func() {
+	// 	fmt.Println(help)
+	// 	flag.PrintDefaults()
+	// }
 
-	// Parse some flags
-	testFile := flag.String("t", "", "Test File")
-	flag.Parse()
+	// // Parse some flags
+	// testFile := flag.String("t", "", "Test File")
+	// flag.Parse()
 
-	if *testFile != "" {
-		if testGravitation(*testFile) {
-			log.Println("Test successful!")
-		} else {
-			log.Println("Test failed.")
-		}
-	} else {
-		testGravitation("test.json")
-	}
+	// if *testFile != "" {
+	// 	if testGravitation(*testFile) {
+	// 		log.Println("Test successful!")
+	// 	} else {
+	// 		log.Println("Test failed.")
+	// 	}
+	// } else {
+	profile := []string{"test", "test2", "test3"}
+	orbit := []Body{}
+	gravitationRendezvous(profile, orbit)
+	// }
+
 }
